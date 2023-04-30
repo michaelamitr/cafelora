74cfa38f9d9ff3274208361a45eb9ff6fe541e5c
Michaela Trčková <misa.trckova@volny.cz>
2023-04-30 15:07:56 +0200

    final edit, homepage created

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import './style.css';
-import { Header } from './components/Header/header.js';
-import { Footer } from './components/Footer/footer.js';
-import { Banner } from './pages/HomePage/components/Banner/banner.js';
-import { Gallery } from './pages/HomePage/components/Gallery/gallery.js';
-import { Contact } from './pages/HomePage/components/Contact/contact.js';
-import { Menu } from './pages/HomePage/components/Menu/menu.js';
+import { HomePage } from './pages/HomePage/homepage.js';
 
-console.log('funguju!');
+const { pathname } = window.location;
 
-const pageElement = document.createElement('div');
-pageElement.classList.add('page');
-
-const main = document.createElement('main');
-main.append(Banner(), Menu({ drinks: 'loading' }), Gallery(), Contact());
-
-pageElement.append(Header(), main, Footer());
-
-document.querySelector('#app').append(pageElement);
+if (pathname === '/') {
+  document.querySelector('#app').append(HomePage());
+}
--- src/pages/HomePage/homepage.js
+++ src/pages/HomePage/homepage.js
@@ -1,4 +1,15 @@
+import { Header } from '../../components/Header/header.js';
+import { Footer } from '../../components/Footer/footer.js';
+import { Banner } from './components/Banner/banner.js';
+import { Gallery } from './components/Gallery/gallery.js';
+import { Contact } from './components/Contact/contact.js';
+import { Menu } from './components/Menu/menu.js';
+
 export const HomePage = () => {
   const homepageElm = document.createElement('div');
+  homepageElm.classList.add('page');
+  const main = document.createElement('main');
+  main.append(Banner(), Menu({ drinks: 'loading' }), Gallery(), Contact());
+  homepageElm.append(Header(), main, Footer());
   return homepageElm;
 };
